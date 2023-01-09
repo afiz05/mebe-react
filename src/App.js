cb17a281defce62a9e5d3131a0b3e55bb0a8fbaf
Reiju_0 <riddwan.a@gmail.com>
2023-01-09 12:19:50 +0700

    push ridwan

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Routes, Route, Link } from "react-router-dom";
 import Home from "./components/templates/Home";
 import Footer from "./components/templates/Footer";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/style/Style.css";
 
 class App extends Component {
   render() {
